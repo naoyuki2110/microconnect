2caaaabe2d3ccc985be01f1ad6834cd151a35619
naoyuki2110 <78297341+naoyuki2110@users.noreply.github.com>
2021-07-11 09:13:32 +0900

    Update header.js

--- js/header.js
+++ js/header.js
@@ -1,14 +1,14 @@
-document.getElementById("hdjs").innerHTML = "  <header>
-    <nav class=&quot;hd&quot;>
-        <ul class=&quot;menu&quot;>
-          <li><img src=&quot;images/mclogocc2.png&quot; width=&quot;50&quot; height=&quot;50&quot;></img></li>
-          <li><a href=&quot;index.html&quot;>TOP</a></li>
-          <li><a href=&quot;terms.html&quot;>利用規約等tgre</a></li>
-          <li><a href=&quot;staff.html&quot;>スタッフerg</a></li>
-          <li><a href=&quot;contact.html&quot;>お問い合わtestせ</a></li>
-          <li><a href=&quot;https://discord.gg/DEf28epDQJ&quot; target=&quot;_blank&quot;>Discord</a></li>
-          <li><a href=&quot;https://twitter.com/pnrmicroconnect/&quot; target=&quot;_blank&quot;>Twitter</a></li>
-          <li><a href=&quot;https://youtube.com/channel/UCKnxqK60oGVlPscRN9YQliw/&quot; target=&quot;_blank&quot;>YouTube</a></li>
+document.getElementById("hdjs").innerHTML = "  <header id="hdjs">
+    <nav class="hd">
+        <ul class="menu">
+          <li><img src="images/mclogocc2.png" width="50" height="50"></img></li>
+          <li><a href="index.html">TOP</a></li>
+          <li><a href="terms.html">利用規約等</a></li>
+          <li><a href="staff.html">スタッフ</a></li>
+          <li><a href="contact.html">お問い合わせ</a></li>
+          <li><a href="https://discord.gg/DEf28epDQJ" target="_blank">Discord</a></li>
+          <li><a href="https://twitter.com/pnrmicroconnect/" target="_blank">Twitter</a></li>
+          <li><a href="https://youtube.com/channel/UCKnxqK60oGVlPscRN9YQliw/" target="_blank">YouTube</a></li>
         </ul>
     </nav>
   </header>";
