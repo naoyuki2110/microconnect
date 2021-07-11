83bf49387d2770104d46490233c622d9dbf2481c
naoyuki2110 <78297341+naoyuki2110@users.noreply.github.com>
2021-07-11 09:05:13 +0900

    Update header.js

--- js/header.js
+++ js/header.js
@@ -1,1 +1,14 @@
-document.getElementById("fvcs").href = "images/mclogocircle.png";
+document.getElementById("hdjs").innerHTML = "  <header>
+    <nav class=&quot;hd&quot;>
+        <ul class=&quot;menu&quot;>
+          <li><img src=&quot;images/mclogocc2.png&quot; width=&quot;50&quot; height=&quot;50&quot;></img></li>
+          <li><a href=&quot;index.html&quot;>TOP</a></li>
+          <li><a href=&quot;terms.html&quot;>利用規約等</a></li>
+          <li><a href=&quot;staff.html&quot;>スタッフ</a></li>
+          <li><a href=&quot;contact.html&quot;>お問い合わせ</a></li>
+          <li><a href=&quot;https://discord.gg/DEf28epDQJ&quot; target=&quot;_blank&quot;>Discord</a></li>
+          <li><a href=&quot;https://twitter.com/pnrmicroconnect/&quot; target=&quot;_blank&quot;>Twitter</a></li>
+          <li><a href=&quot;https://youtube.com/channel/UCKnxqK60oGVlPscRN9YQliw/&quot; target=&quot;_blank&quot;>YouTube</a></li>
+        </ul>
+    </nav>
+  </header>";
