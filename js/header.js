0598c66570b9657df3ec5447fa11054c3ff33d4c
naoyuki2110 <78297341+naoyuki2110@users.noreply.github.com>
2021-07-11 15:06:01 +0900

    Update header.js

--- js/header.js
+++ js/header.js
@@ -1,5 +1,5 @@
 window.onload = function(){
-  document.getElementById("hdjs").innerHTML = "<header>
+  document.getElementById("hdjs").innerHTML = "<header id="hdjs">
   <nav class='hd'>
     <ul class='menu'><li><img src='images/mclogocc2.png' width='50' height='50'></img></li><li><a href='index.html'>TOP</a></li><li><a href='staff.html'>スタッフ一覧</a></li><li><a href='#'>ソーシャル</a><ul class='menu2'><li><a href='https://discord.gg/DEf28epDQJ' target='_blank'>Discord</a></li><li><a href='https://twitter.com/pnrmicroconnect/' target='_blank'>Twitter</a></li><li><a href='https://youtube.com/channel/UCKnxqK60oGVlPscRN9YQliw/' target='_blank'>YouTube</a></li></ul></li><li><a href='https://discord.gg/DEf28epDQJ' target='_blank'>Discord</a></li><li><a href='https://twitter.com/pnrmicroconnect/' target='_blank'>Twitter</a></li><li><a href='https://youtube.com/channel/UCKnxqK60oGVlPscRN9YQliw/' target='_blank'>YouTube</a></li></ul></nav></header>";
 }
